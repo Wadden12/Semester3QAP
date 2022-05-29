9c2a2267d7066318711ebded2faed27034ff8bbf
Mike Wadden <mike.wadden@keyin.com>
2022-05-29 12:28:28 -0230

    Added the more files

--- NODEQAP2/index.js
+++ NODEQAP2/index.js
@@ -1,3 +1,9 @@
 // Date May 27th
 //Author Mike Wadden
 // File: QAP 2 for Node
+
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  let path = require("./views/");
+});
