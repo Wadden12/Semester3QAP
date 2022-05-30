b8ad208ed6a06310015480f5973197d85bb8d05b
Mike Wadden <mike.wadden@keyin.com>
2022-05-29 21:54:42 -0230

    Not working right atm

---
+++ NODEQAP2/logger.js
@@ -0,0 +1,35 @@
+const EventEmitter = require("events");
+const uuid = require("uuid");
+path = require("path");
+fs = require("fs");
+
+class Logger extends EventEmitter {
+  //   log(message) {
+  //     // Send an HTTP Request example
+  //     console.log(message);
+  //     this.emit("messageLogged", { id: 1, url: "http://test" });
+  //   }
+
+  // creates a function to log events to a text file
+  logEvent(route, level, message) {
+    let logItem = null;
+    const date = new Date();
+    if (level === "error") {
+      logItem = `Date:${date.toLocaleString()} Token: ${uuid.v4()} ${level.toUpperCase()} ${route}.html  ${message}\n`;
+      console.log(logItem);
+    } else {
+      logItem = `Date: ${date.toLocaleString()}  Token: ${uuid.v4()} ${level.toUpperCase()} ${route}.html  ${message}\n`;
+      console.log(logItem);
+    }
+
+    fs.appendFile("Log/logger.txt", logItem, (err) => {
+      if (err) {
+        throw err;
+      } else {
+        this.emit("route", ` ${route}.html Logged Saved`);
+      }
+    });
+  }
+}
+
+module.exports = Logger;
--- NODEQAP2/routes.js
+++ NODEQAP2/routes.js
@@ -1,29 +1,65 @@
 const display = require("./display");
+const Logger = require("./logger");
+const logger = new Logger();
+
+//regiester a listener
+
+// logger.on("messageLogged", (arg) => {
+//   console.log("Listener Called", arg);
+// });
+// logger.log("message");
+
+// path route functions for each page
 
 let index = (path, response) => {
+  logger.logEvent("index", "info", "the home page for the site was visted");
   display.displayFile(path, response);
+  logger.on("route", (arg) => {
+    console.log("Listener Called", arg);
+  });
 };
 
 let about = (path, response) => {
+  logger.logEvent("about", "info", "the about page was visted");
   display.displayFile(path, response);
+  logger.on("route", (arg) => {
+    console.log("Listener Called", arg);
+  });
 };
 
 let gallery = (path, response) => {
+  logger.logEvent("gallery", "info", "the gallery page was visted");
   display.displayFile(path, response);
 };
+logger.on("route", (arg) => {
+  console.log("Listener Called", arg);
+});
 
 let subscribe = (path, response) => {
+  logger.logEvent("subscribe", "info", "the subscribe page was visted");
   display.displayFile(path, response);
 };
+logger.on("route", (arg) => {
+  console.log("Listener Called", arg);
+});
 
 let contact = (path, response) => {
+  logger.logEvent("contact", "info", "the contact page was visted");
   display.displayFile(path, response);
 };
+logger.on("route", (arg) => {
+  console.log("Listener Called", arg);
+});
 
 let notFound = (url, path, response) => {
+  logger.logEvent(url, "error", "route was not found");
   display.displayFile(path, response);
 };
+logger.on("route", (arg) => {
+  console.log("Listener Called", arg);
+});
 
+// export functions to be used on another module
 module.exports = {
   index,
   about,
